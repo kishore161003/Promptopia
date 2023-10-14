43e105a0cc4bf6f5881e18b9977fdbb4531b3d41
kishore161003 <kishorekumarapandianp21cs@psnacet.edu.in>
2023-10-14 17:59:56 +0530

    final

--- model/user.js
+++ model/user.js
@@ -11,7 +11,6 @@ const UserSchema = new Schema({
     type: String,
     required: [true, "Username is required!"],
     match: [
-      /^(?=.{8,40}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
       "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
     ],
   },
