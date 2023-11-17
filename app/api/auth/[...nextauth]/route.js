b4168056fd8dbb09d3afd7021653d0e0001a8444
kishore161003 <kishorekumarapandianp21cs@psnacet.edu.in>
2023-11-17 14:54:14 +0530

    finished

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,9 @@
-import nextAuth from "next-auth";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { connectToDB } from "@utils/database";
-import User from "@model/user";
+import { connectTodDB } from "@/utils/database";
+import User from "@/models/user";
 
-const handler = nextAuth({
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
@@ -12,24 +12,31 @@ const handler = nextAuth({
   ],
   callbacks: {
     async session({ session }) {
-      const sessionuser = await User.findOne({
-        email: session.user.email,
-      });
-
-      session.user.id = sessionuser._id.toString();
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
       return session;
     },
-
-    async redirect({ url, baseUrl }) {
-      return baseUrl;
-    },
-
     async signIn({ profile }) {
       try {
-        await connectToDB();
-        if (!(await User.findOne({ email: profile.email }))) {
-          console.log(profile.name);
-          // if not, create user in database
+        await connectTodDB();
+        console.log(
+          await User.findOne({
+            email: profile.email,
+          })
+        );
+        // if not , create a new user
+        if (
+          !(await User.findOne({
+            email: profile.email,
+          }))
+        ) {
+          console.log({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image:
+              profile.picture ||
+              "https://res.cloudinary.com/dq7l8216n/image/upload/v1622711879/nextjs-mongodb-cloudinary/placeholder-image.jpg",
+          });
           await User.create({
             email: profile.email,
             username: profile.name.replace(" ", "").toLowerCase(),
@@ -37,11 +44,10 @@ const handler = nextAuth({
               profile.picture ||
               "https://res.cloudinary.com/dq7l8216n/image/upload/v1622711879/nextjs-mongodb-cloudinary/placeholder-image.jpg",
           });
-          return true;
         }
-      } catch (error) {
-        console.log(error);
-        return false;
+        return true;
+      } catch (e) {
+        console.log(e);
       }
     },
   },
