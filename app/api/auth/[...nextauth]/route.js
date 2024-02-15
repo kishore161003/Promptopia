14f2ceb8bc83712f70d8eeba12c5d631957801e2
kishore161003 <kishorekumarapandianp21cs@psnacet.edu.in>
2024-02-15 14:42:44 +0530

    finally Completed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,10 +3,10 @@ import GoogleProvider from "next-auth/providers/google";
 import { connectToDatabase } from "@utils/database";
 import User from "@models/user";
 
-const handler =  NextAuth({
+const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
@@ -20,14 +20,16 @@ const handler =  NextAuth({
       try {
         await connectToDatabase();
         // check if user exists in database
-        if(await User.findOne({ email: profile.email })) {
+        if (await User.findOne({ email: profile.email })) {
           return true;
         }
         // if not, create user in database
         await User.create({
           email: profile.email,
           username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture || "https://res.cloudinary.com/dq7l8216n/image/upload/v1622711879/nextjs-mongodb-cloudinary/placeholder-image.jpg",
+          image:
+            profile.picture ||
+            "https://res.cloudinary.com/dq7l8216n/image/upload/v1622711879/nextjs-mongodb-cloudinary/placeholder-image.jpg",
         });
 
         return true;
@@ -35,8 +37,8 @@ const handler =  NextAuth({
         console.log(err);
         return false;
       }
-    }
-  }
+    },
+  },
 });
 
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST };
